4b48892ae668730bb4676a306fa786f165fed83c
tommy6073 <tommyt6073@gmail.com>
2017-07-01 12:08:39 +0900

    Remove .bash_profile and .zshrc

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
