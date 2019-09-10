fb8400fd762df8554eddad967cd1ee83650e3ca8
tommy6073 <tommyt6073@gmail.com>
2019-09-10 11:14:05 +0900

    Add rbenv setting to config.fish

--- config.fish
+++ config.fish
@@ -1,2 +1,7 @@
 alias ocaml="rlwrap ocaml"
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+status --is-interactive; and source (rbenv init -|psub)
+
