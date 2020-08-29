5fc1b61941acadafa37598f0bc2b0cdb5805738b
tommy6073 <tommyt6073@gmail.com>
2020-08-29 22:01:45 +0900

    Change shell to zsh from fish

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
