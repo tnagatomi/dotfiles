74d5b7778967aa8eef9010ed1ec42941a202b831
tommy6073 <tommyt6073@gmail.com>
2020-09-03 07:59:52 +0900

    Change gvim font to MesloLGS NF

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -2,4 +2,5 @@
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
