11b3436458f2efdb3ca8d0d01ce2200e0543fdc1
Takayuki Nagatomi <tommyt6073@gmail.com>
2023-09-10 13:07:12 +0900

    Remove gvim configs

--- .gvimrc
+++
@@ -1,1 +0,0 @@
-set guifont=MesloLGS\ NF:h13
--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -3,6 +3,5 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.asdfrc ~/.asdfrc
