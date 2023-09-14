f6752c3f00194defbc99aea7ef8e978d960ddca3
Takayuki Nagatomi <tommyt6073@gmail.com>
2023-09-14 16:14:06 +0900

    Remove kitty config

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -5,4 +5,3 @@ ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.asdfrc ~/.asdfrc
-ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
