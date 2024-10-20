d48c4479516c30e952115f02c9516122f83b3be0
Takayuki Nagatomi <tommyt6073@gmail.com>
2024-10-21 07:13:07 +0900

    Setup Neovim

---
+++ .config/nvim/lua/autocmds.lua
@@ -0,0 +1,9 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Restore cursor position
+autocmd({ "BufReadPost" }, {
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_exec('silent! normal! g`"zv', false)
+  end,
+})
