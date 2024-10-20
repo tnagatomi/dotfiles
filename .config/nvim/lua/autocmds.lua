cd9f5d31346cdfaa26b598eb46b986e57d74a909
Takayuki Nagatomi <tommyt6073@gmail.com>
2024-10-21 08:40:30 +0900

    Revert "Setup Neovim"
    This reverts commit d48c4479516c30e952115f02c9516122f83b3be0.

--- .config/nvim/lua/autocmds.lua
+++
@@ -1,9 +0,0 @@
-local autocmd = vim.api.nvim_create_autocmd
-
--- Restore cursor position
-autocmd({ "BufReadPost" }, {
-  pattern = { "*" },
-  callback = function()
-    vim.api.nvim_exec('silent! normal! g`"zv', false)
-  end,
-})
--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.wezterm.lua ~/.wezterm.lua
 
-mkdir -p ~/dotfiles/.config
+if [ -d ~/dotfiles/.config ]
+then
+    mkdir -p ~/dotfiles/.config
+fi
 
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/.config/starship.theme.toml ~/.config/starship.theme.toml
-
-src_dir="${HOME}/dotfiles/.config/nvim"
-dest_dir="${HOME}/.config/nvim"
-mkdir -p "$dest_dir"
-find "$src_dir" -type f | while read src_file; do
-  dest_file="$dest_dir/$(grealpath --relative-to="$src_dir" "$src_file")"
-
-  mkdir -p "$(dirname "$dest_file")"
-
-  ln -sf "$src_file" "$dest_file"
-done
