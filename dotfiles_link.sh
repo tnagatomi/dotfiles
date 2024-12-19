6110c0d1e84cf4ff6490aae3d41f57955174d7e2
Takayuki Nagatomi <tommyt6073@gmail.com>
2024-12-19 09:35:35 +0900

    Delete .p10k.zsh

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.wezterm.lua ~/.wezterm.lua
