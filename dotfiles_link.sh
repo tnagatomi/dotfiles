d0100039794354a7996fbdf82db5e066b1b40f91
Takayuki Nagatomi <tommyt6073@gmail.com>
2024-12-22 06:35:34 +0900

    Delete starshihp.theme.toml since it's not used

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -5,10 +5,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.wezterm.lua ~/.wezterm.lua
 
-if [ -d ~/dotfiles/.config ]
-then
-    mkdir -p ~/dotfiles/.config
-fi
+mkdir -p ~/dotfiles/.config
 
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-ln -sf ~/dotfiles/.config/starship.theme.toml ~/.config/starship.theme.toml
