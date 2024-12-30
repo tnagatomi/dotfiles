63995b937165dfe622c3a5e7301a6b8a479a7283
Takayuki Nagatomi <tommyt6073@gmail.com>
2024-12-30 13:28:42 +0900

    Remove WezTerm config

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -3,7 +3,6 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.wezterm.lua ~/.wezterm.lua
 
 mkdir -p ~/.config
 
