4871f2b4b8b3cbff9f9ef93c6106f461393ec68a
Takayuki Nagatomi <tommyt6073@gmail.com>
2025-01-20 09:44:38 +0900

    Fix mkdir

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-mkdir -p ~/dotfiles/.local/share/zinit/completions
+mkdir -p ~/.local/share/zinit/completions
 ln -sf ~/dotfiles/.local/share/zinit/completions/_zellij ~/.local/share/zinit/completions/_zellij
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
-mkdir -p ~/dotfiles/.config
+mkdir -p ~/.config
 
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 
-mkdir -p ~/dotfiles/.config/ghostty
+mkdir -p ~/.config/ghostty
 ln -sf ~/dotfiles/.config/ghostty/config ~/.config/ghostty/config
 
-mkdir -p ~/dotfiles/.config/zellij
+mkdir -p ~/.config/zellij
 ln -sf ~/dotfiles/.config/zellij/config.kdl ~/.config/zellij/config.kdl
