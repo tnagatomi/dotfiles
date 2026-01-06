8373c458f6dde46a939e09505dd3d5a183dff119
Takayuki Nagatomi <tnagatomi@okweird.net>
2026-01-06 09:27:26 +0900

    Add back WezTerm config

---
+++ .wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "catppuccin-mocha"
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.font = wezterm.font 'UDEV Gothic 35NF'
+config.font_size = 13.0
+config.tab_max_width = 32
+
+config.keys = {
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {
+    key = "LeftArrow",
+    mods = "OPT",
+    action = wezterm.action { SendString="\x1bb" }
+  },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+  {
+    key = "RightArrow",
+    mods = "OPT",
+    action = wezterm.action { SendString="\x1bf" }
+  },
+  {
+    key="Enter",
+    mods="SHIFT",
+    action=wezterm.action{SendString="\x1b\r"}
+  },
+}
+
+return config
--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.wezterm.lua ~/.wezterm.lua
 
 mkdir -p ~/.config
 
