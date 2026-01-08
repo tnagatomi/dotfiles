f40ddcf81c881d4d024fb06cef024fdbbed1a877
Takayuki Nagatomi <tnagatomi@okweird.net>
2026-01-08 12:00:41 +0900

    Revert "Remove Shift+Enter binding for Claude Code since it's no longer necessary"
    This reverts commit e71d59b667762ed005e424725ece6d53c00836a5.

--- .wezterm.lua
+++ .wezterm.lua
@@ -33,6 +33,11 @@ config.keys = {
     mods = "OPT",
     action = wezterm.action { SendString="\x1bf" }
   },
+  {
+    key="Enter",
+    mods="SHIFT",
+    action=wezterm.action{SendString="\x1b\r"}
+  },
 }
 
 return config
